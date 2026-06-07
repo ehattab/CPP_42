309714fae45cfaf77e9711329e4aaa86f692878b
ehattab <ehattab@student.42.fr>
2026-06-07 17:34:34 +0200

    test2

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: usr <usr@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 19:19:44 by ehattab           #+#    #+#             */
-/*   Updated: 2026/06/02 15:54:21 by usr              ###   ########.fr       */
+/*   Updated: 2026/06/07 17:08:50 by usr              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@ static std::string convert(std::string value)
 			i++;
 		if (value[i] >= 10 && value[i] <= 127);
 		if (value[i] == '.' || value[i] == 'f');
-		
+		else
+			return value + " is not possible";
 	}
+	return value;
 }
