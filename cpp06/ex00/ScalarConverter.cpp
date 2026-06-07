e985db3be2fd1be9415fff3282071acf3204a2c0
ehattab <hattab.elies@gmail.com>
2026-06-07 17:08:37 +0200

    test

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ehattab <ehattab@student.42.fr>            +#+  +:+       +#+        */
+/*   By: usr <usr@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 19:19:44 by ehattab           #+#    #+#             */
-/*   Updated: 2026/06/01 21:02:35 by ehattab          ###   ########.fr       */
+/*   Updated: 2026/06/02 15:54:21 by usr              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,7 @@ static std::string convert(std::string value)
 		if (value[i] == '-' || value[i] == '+')
 			i++;
 		if (value[i] >= 10 && value[i] <= 127);
+		if (value[i] == '.' || value[i] == 'f');
+		
 	}
 }
